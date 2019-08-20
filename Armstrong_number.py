59da3c9be03346d22cd98e6b8c0d598121657e6e
nishant-passari <53423387+nishant-passari@users.noreply.github.com>
2019-08-20 17:06:50 +0530

    Add files via upload

---
+++ Armstrong_number.py
@@ -0,0 +1,20 @@
+Num = list(range(100, 501))      # list of numbers from 100-500
+armstrong = []                   # empty list to append the armstrong numbers
+
+def Armstrong(Num):              # armstrong function
+
+    """ This function calculates the armstrong number in the given list. """
+
+    for i in Num:                # for loop to iterate over the list
+        n_ = n = i
+        sum_ = 0    
+        while n != 0:            # run loop until n == 0
+            n = n//10            # calculating the quotient
+            r = n_ % 10          # calculating the remainder
+            sum_ += r**3         # calculating cube of the remainder and adding it to s
+            n_ = n               # updating value of n to n_
+        if sum_ == i:            # if condition to append the armstrong numbers into the  list
+            armstrong.append(i)
+    return armstrong             # returning the armstrong list
+
+print(Armstrong(Num))            # calling the armstrong function
